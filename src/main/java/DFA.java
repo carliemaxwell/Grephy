324ecd79e569c3f75ef3fdc66575443fc838e46b
Carlie Maxwell <carliemaxwell@aol.com>
2018-03-21 16:01:01 -0400

    returns eclosure of state and properly constructs the start state/return state of a symbol

--- src/main/java/DFA.java
+++ src/main/java/DFA.java
@@ -2,27 +2,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class DFA {
 
-    public List<Transition> dfaTransitions;
+    public List<DFATransition> dfaTransitions;
     public List<Integer> dfaStates = new ArrayList<Integer>();
-    public int acceptingState;
-    private static String file;
-    //    public static String alphabet = "";
-    public static List<String> alphabet = new ArrayList<String>();
-    private static char character;
+    public List<Integer> acceptingState;
+    public static HashMap<Integer, List> statesAndEclosures = new HashMap<Integer, List>();
 
 
     //used for creating an empty NFA to combine 2 or more NFA's
     public DFA() {
         //NFA consists of a series of transitions and states
-        dfaTransitions = new ArrayList<Transition>();
+        dfaTransitions = new ArrayList<DFATransition>();
         dfaStates = new ArrayList<Integer>();
         //depends on first+secondSize-1
-        this.acceptingState = 0;
+        this.acceptingState = new ArrayList<Integer>();
     }
 
 
@@ -37,63 +35,80 @@ public class DFA {
         Transition currentTransition = null;
         List<List<Integer>> eclosureSetGlobal = new ArrayList<List<Integer>>();
 
-        for(int a = 0; a < nfa.states.size(); a++) {
+        for (int a = 0; a < nfa.states.size(); a++) {
             List<Integer> eclosureSet = new ArrayList<Integer>();
             eclosureSet.add(a);
             for (int x = 0; x < nfa.transitions.size(); x++) {
                 if (nfa.transitions.get(x).prior == a) {
                     currentTransition = nfa.transitions.get(x);
                     if (currentTransition.label == 'e') {
-                        if(!eclosureSet.contains(currentTransition.next)) {
+                        if (!eclosureSet.contains(currentTransition.next)) {
                             eclosureSet.add(currentTransition.next);
                         }
                     }
                 }
             }
+            statesAndEclosures.put(eclosureSet.get(0), eclosureSet);
             eclosureSetGlobal.add(eclosureSet);
             System.out.println(eclosureSet);
         }
-        System.out.println(eclosureSetGlobal);
+        System.out.println(statesAndEclosures);
         return eclosureSetGlobal;
     }
 
+    public static List<Integer> returnEclosureSet(int state) {
+        List eclosureOfState = statesAndEclosures.get(state);
+        return eclosureOfState;
+    }
 
-    public void transitionTable(NFA nfa, List<List<Integer>> globalEnclosures, String regex) throws IOException {
+    public static DFA createDFA(NFA nfa) {
 
-        //need to move bc want 1 list for all 3 unions
-        List<Character> regexCharacters = Transition.returnRegexCharacters(regex);
-        List<Integer> firstTransition = new ArrayList<Integer>();
+        DFA dfa = new DFA();
 
-        //move bc want one for each state + symbol
-        //maybe make 2D array for a matrix?
+        //Get start state of NFA
+        int startState = nfa.states.get(0);
 
+        //Get eclosure of start state to begin constructing transitions
+        List<Integer> eclosure = returnEclosureSet(startState);
 
-        //needed incase you start with concat (won't be epsilon transition)
-        for(int x = 0; x < globalEnclosures.size(); x++) {
-            if(globalEnclosures.get(x).size() > 1) {
-                firstTransition = globalEnclosures.get(x);
-            }
-        }
+        System.out.println("Start state eclosure is " + eclosure);
 
-
-        for(int a = 0; a < regexCharacters.size(); a++) {
-            //need to add to bigger thing bc this is individual for symbol + state
-            List<Integer> returnTransition = new ArrayList<Integer>();
-            //want to go through each state in list and find their mapping in nfa transitions
-            for (int y = 0; y < firstTransition.size(); y++) {
+        for (int y = 0; y < Ingestion.alphabet.size(); y++) {
+            //Get the first symbol in the alphabet
+            char currentChar = Ingestion.alphabet.get(y);
+            System.out.println("current character is " + currentChar);
+            List<Integer> returnedStates = new ArrayList<Integer>();
+            //Get each value in the eclosure set
+            for (int x = 0; x < eclosure.size(); x++) {
+                System.out.println("Current value in eclosure set " + eclosure.get(x));
+                //Check if the transitions have that label
                 for (int z = 0; z < nfa.transitions.size(); z++) {
-                    //find a list of 0's in nfa --> list of 1's --> list of 4's
-                    if (nfa.transitions.get(z).prior == firstTransition.get(y)) {
-                        //need to fix regexCharacters (maybe another loop)
-                        if (nfa.transitions.get(z).label == 'e' ||
-                                nfa.transitions.get(z).label == regexCharacters.get(a)) {
-                            returnTransition.add(nfa.transitions.get(z).next);
+                    if (nfa.transitions.get(z).prior == eclosure.get(x)) {
+                        if (nfa.transitions.get(z).label == currentChar) {
+                            if (!returnedStates.contains(nfa.transitions.get(x).next)) {
+                                List<Integer> eclosureOfNextState = returnEclosureSet(nfa.transitions.get(z).next);
+                                System.out.println("State is " + nfa.transitions.get(z).next + " eclosure is " + eclosureOfNextState);
+                                for (int a = 0; a < eclosureOfNextState.size(); a++) {
+                                    returnedStates.add(eclosureOfNextState.get(a));
+                                }
+                            }
                         }
+//                        else {
+//
+//                            add a trap state
+//                            if (!returnedStates.contains(nfa.states.size())) {
+//                                returnedStates.add(nfa.states.size());
+//                            }
+//
                     }
                 }
+                dfa.dfaTransitions.add(new DFATransition(eclosure, returnedStates, currentChar));
+                System.out.println(dfa.dfaTransitions.get(y).prior + " " + dfa.dfaTransitions.get(y).next + " " + dfa.dfaTransitions.get(y).label);
             }
         }
+        return dfa;
     }
 }
+
 
 
