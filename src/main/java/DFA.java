6ffd64ecd37dcfbe5086576e6dd4bc0a81ad3ecd
Carlie Maxwell <carliemaxwell@aol.com>
2018-03-21 19:11:27 -0400

    added global list of dfa states

--- src/main/java/DFA.java
+++ src/main/java/DFA.java
@@ -1,6 +1,4 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -65,20 +63,30 @@ public class DFA {
 
         DFA dfa = new DFA();
 
+        List<List<Integer>> dfaStates = new ArrayList<>();
+
         //Get start state of NFA
         int startState = nfa.states.get(0);
 
         //Get eclosure of start state to begin constructing transitions
-        List<Integer> eclosure = returnEclosureSet(startState);
+//        List<Integer> eclosure = returnEclosureSet(startState);
 
-        System.out.println("Start state eclosure is " + eclosure);
+
+        //Create list of all the states ahead of time and then run that list through the outer alphabet loop?
+
 
         for (int y = 0; y < Ingestion.alphabet.size(); y++) {
-            //Get the first symbol in the alphabet
             char currentChar = Ingestion.alphabet.get(y);
-            System.out.println("current character is " + currentChar);
+
             List<Integer> returnedStates = new ArrayList<Integer>();
-            //Get each value in the eclosure set
+
+            List<Integer> eclosure = returnEclosureSet(startState);
+
+            //want to make global list of all states in DFA
+            if(!dfaStates.contains(eclosure)) {
+                dfaStates.add(eclosure);
+            }
+
             for (int x = 0; x < eclosure.size(); x++) {
                 System.out.println("Current value in eclosure set " + eclosure.get(x));
                 //Check if the transitions have that label
@@ -100,12 +108,51 @@ public class DFA {
                 //-1 = error state
                 returnedStates.add(-1);
             }
+
             dfa.dfaTransitions.add(new DFATransition(eclosure, returnedStates, currentChar));
+
+            if(returnedStates != eclosure && !dfaStates.contains(returnedStates)) {
+                dfaStates.add(returnedStates);
+            }
         }
+
         for(int b = 0; b < dfa.dfaTransitions.size(); b++) {
             System.out.println(dfa.dfaTransitions.get(b).prior + " " + dfa.dfaTransitions.get(b).next + " " + dfa.dfaTransitions.get(b).label);
         }
+
+        System.out.println("DFA states " + dfaStates);
+
         return dfa;
+    }
+
+
+    public static void writeToFileDFA(DFA dfa) throws IOException {
+        BufferedWriter bw = null;
+        try {
+            String dotLanguage = "digraph graphname { ";
+            File file = new File("/Users/carliemaxwell/GrephyFinalProject/src/main/Output/Output");
+            FileWriter fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            bw.write(dotLanguage);
+            bw.newLine();
+            for(int x=0; x< dfa.dfaTransitions.size(); x++) {
+                //NEED TO FIX TO BE A STRING FOR EACH STATE
+                bw.write(String.join(",",dfa.dfaTransitions.get(x).prior) + "->" +
+                        String.join(",",dfa.dfaTransitions.get(x).next) + "[label=" + dfa.dfaTransitions.get(x).label + "];");
+                bw.newLine();
+            }
+            bw.write("}");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (Exception ex) {
+                System.out.println("Error");
+            }
+        }
     }
 }
 
