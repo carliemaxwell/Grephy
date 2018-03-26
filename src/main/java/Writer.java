f54357ce518357e6958788dc2172ec82a3e53b6e
Carlie Maxwell <carliemaxwell@aol.com>
2018-03-26 12:30:29 -0400

    New file for writing to output for NFA and DFA

---
+++ src/main/java/Writer.java
@@ -0,0 +1,78 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writer {
+
+
+
+    public static void writeToFileNFA(NFA nfa) throws IOException {
+        BufferedWriter bw = null;
+        try {
+            String dotLanguage = "digraph graphname { ";
+            File file = new File("/Users/carliemaxwell/GrephyFinalProject/src/main/Output/Output");
+            FileWriter fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            bw.write(dotLanguage);
+            bw.newLine();
+            for(int x=0; x< nfa.transitions.size(); x++) {
+                bw.write(nfa.transitions.get(x).prior + "->" +
+                        nfa.transitions.get(x).next + "[label=" + nfa.transitions.get(x).label + "];");
+                bw.newLine();
+            }
+            bw.write("}");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (Exception ex) {
+                System.out.println("Error");
+            }
+        }
+    }
+
+    public static void writeToFileDFA(DFA dfa) throws IOException {
+        BufferedWriter bw = null;
+        try {
+            String dotLanguage = "digraph graphname { ";
+            File file = new File("/Users/carliemaxwell/GrephyFinalProject/src/main/Output/Output");
+            FileWriter fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            bw.write(dotLanguage);
+            bw.newLine();
+            for(int x=0; x< dfa.dfaTransitions.size(); x++) {
+
+                StringBuilder builder2 = new StringBuilder();
+                for (int m = 0; m < dfa.dfaTransitions.get(x).next.size(); m++) {
+                    builder2.append( dfa.dfaTransitions.get(x).next.get(m));
+                }
+                String result2 = builder2.toString();
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < dfa.dfaTransitions.get(x).prior.size(); i++) {
+                    builder.append( dfa.dfaTransitions.get(x).prior.get(i));
+                }
+                String result = builder.toString();
+
+                //NEED TO FIX TO BE A STRING FOR EACH STATE
+                bw.write(result + "->" + result2 + "[label=" + dfa.dfaTransitions.get(x).label + "];");
+                bw.newLine();
+            }
+            bw.write("}");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (Exception ex) {
+                System.out.println("Error");
+            }
+        }
+    }
+}
