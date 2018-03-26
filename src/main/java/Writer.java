f8cfa91f41949071925552493c97139ce4ec2175
Carlie Maxwell <carliemaxwell@aol.com>
2018-03-26 15:56:33 -0400

    Fixed writeToFileDFA to find the accepting state and make a double circle in DOT language

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -2,6 +2,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Writer {
 
@@ -44,22 +46,35 @@ public class Writer {
             bw = new BufferedWriter(fw);
             bw.write(dotLanguage);
             bw.newLine();
-            for(int x=0; x< dfa.dfaTransitions.size(); x++) {
-
-                StringBuilder builder2 = new StringBuilder();
-                for (int m = 0; m < dfa.dfaTransitions.get(x).next.size(); m++) {
-                    builder2.append( dfa.dfaTransitions.get(x).next.get(m));
-                }
-                String result2 = builder2.toString();
+            for (int x = 0; x < dfa.dfaTransitions.size(); x++) {
 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < dfa.dfaTransitions.get(x).prior.size(); i++) {
-                    builder.append( dfa.dfaTransitions.get(x).prior.get(i));
+                    builder.append(dfa.dfaTransitions.get(x).prior.get(i));
                 }
                 String result = builder.toString();
 
+
+                StringBuilder builder2 = new StringBuilder();
+                for (int m = 0; m < dfa.dfaTransitions.get(x).next.size(); m++) {
+                    builder2.append(dfa.dfaTransitions.get(x).next.get(m));
+                }
+                String result2 = builder2.toString();
+
                 //NEED TO FIX TO BE A STRING FOR EACH STATE
                 bw.write(result + "->" + result2 + "[label=" + dfa.dfaTransitions.get(x).label + "];");
+                bw.newLine();
+            }
+            //switch accepting state back to List<List>>
+            for(int k = 0; k < dfa.acceptingState.size(); k++) {
+                StringBuilder builder3 = new StringBuilder();
+                System.out.println(dfa.acceptingState.get(k));
+                for(int g = 0; g < dfa.acceptingState.get(k).size(); g++){
+                    builder3.append(dfa.acceptingState.get(k).get(g));
+                }
+                String result3 = builder3.toString();
+                System.out.println(result3);
+                bw.write(result3 + "[shape=doublecircle];");
                 bw.newLine();
             }
             bw.write("}");
